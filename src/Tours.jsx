c6e340861acb0b982ebf30ee38224c8ef6a508f3
Mehul Mohan <mehulmpt@gmail.com>
2021-08-19 07:28:38 +0530

    Update Tours.jsx

--- src/Tours.jsx
+++ src/Tours.jsx
@@ -2,20 +2,19 @@ import React from 'react'
 import Tour from './Tour'
 
 const Tours = (props) => {
-	// take out tours from props
-
-	return (
-		<section>
-			<div className="title">
-				<h2>All Tours</h2>
-				<div className="underline"></div>
-			</div>
-			<div>
-				{/* map over tours and display UI */}
-				{/* read instructions how UI should look */}
-			</div>
-		</section>
-	)
+    // take out tours from props
+    const { tours } = props;
+    return (
+        <section>
+            <div className="title">
+                <h2>All Tours</h2>
+                <div className="underline"></div>
+            </div>
+            <div>
+                {tours.map(tour => <Tour {...tour} key={tour.id} />)}
+            </div>
+        </section>
+    )
 }
 
 export default Tours
