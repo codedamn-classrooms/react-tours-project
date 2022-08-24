6714bf9cef9d55a8105521d6dd8b0fa2251a4590
Pranav <pranav.techiegeek@gmail.com>
2022-08-24 10:16:55 +0530

    fix(react-dom/client)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
-	const [tours, setTours] = useState([])
+  const [tours, setTours] = useState([]);
 
-	async function fetchTours() {
-		// write code to fetch tours here
-	}
+  async function fetchTours() {
+    // write code to fetch tours here
+  }
 
-	return tours.map((tour) => {
-		return <h2>{tour.name}</h2>
-	})
+  return tours.map((tour) => {
+    return <h2>{tour.name}</h2>;
+  });
 }
 
-export default App
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
-)
+const component = document.getElementById("root");
+
+const root = ReactDOM.createRoot(component);
+
+root.render(<App />);
